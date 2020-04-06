f4721ac1e0b4030161408c224643f16bdb6da3f6
drossos <daniel.rossos@mail.utoronto.ca>
2020-04-06 15:09:37 -0400

    normalized and better auto fill

--- image_distortion.py
+++ image_distortion.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import math 
 import matplotlib.pyplot as plt
+from operator import add
 
 distored_link = "images/distorted"
 undistored_link = "images/undistorted"
@@ -18,31 +19,51 @@ img = cv2.imread(undistored_link +"/"+img_pick+".jpg")
 imgundist = cv2.imread(distored_link +"/"+img_undistpick+".jpg")
 
 #distortion params
-K1=0.00000000001
-K2=0.0000000000000001
-K3=0.00000000000000001
+K1=.4
+K2=.05
+K3=.03
+
+name = "K1="+str(K1)+"_K2="+str(K2)+"_K3="+str(K3)
+
+#normalization
+if (K1 >= 0 and K2 >= 0 and K3 >= 0):
+    r_max = math.sqrt(2)
+else:
+    r_max = math.sqrt(1)
+x_scale = 1+K1*r_max**2 + K2*r_max**4 + K3*r_max**6
+y_scale = x_scale
 
 def distort_image(img):
     h,w,d = img.shape
     #scallling factor
-    sf = 2
+    sf = 1
     distort = np.zeros(shape=[round(h*sf), round(w*sf), 3], dtype=np.uint8)
-    r_arr= []
+    x_cent = w//2
+    y_cent = h//2
+    transformed_points = []
+
     for y in range(0,h):
         for x in range(0,w):
-            xcorr = x-w//2
-            ycorr = y-h//2
-            r = math.sqrt((xcorr)**2 + (ycorr)**2)
-            try:
-                distort[cord_distort(ycorr,r)+h*sf//2][cord_distort(xcorr,r)+w*sf//2] = img[y][x]
-            except:
-                print("index out of bound - distort_image")
-    
-    
-    cv2.imshow("prev", distort)
-    cv2.waitKey(0) # waits until a key is pressed
-    cv2.destroyAllWindows() # destroys the window showing image
+            x_norm = (x-x_cent)/x_cent
+            y_norm = (y-y_cent)/y_cent 
+            r = np.sqrt(x_norm**2 + y_norm**2)
+            x_dist_norm = cord_distort(x_norm,r)
+            y_dist_norm = cord_distort(y_norm,r)
+            x_distorted = int(x_dist_norm*x_cent + x_cent) 
+            y_distorted = int(y_dist_norm*y_cent + y_cent)
+            
+            distort[y_distorted][x_distorted]=img[y][x]
 
+            itters = ((0,1),(0,2),(1,0),(2,0))
+            for i in itters:
+                try:
+                    transformed_points.append(list( map(add,(y_distorted,x_distorted),i)))
+                except:
+                    print("OOB")
+
+    cv2.imshow("test",distort)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
     unfil_copy = distort.copy()
     #writting to file - no fill
@@ -52,67 +73,71 @@ def distort_image(img):
         position,cv2.FONT_HERSHEY_SIMPLEX,1,(0, 255, 0), 4)
     cv2.imwrite(distored_link +"/"+img_pick+name+"_nofill.jpg", wtext)
 
-    box_to_fill = ((w*sf//2 - w//2 - 300//sf,h*sf//2 - h//2- 300//sf),
-        (w//2 + 300//sf + w*sf//2, h//2 + 300//sf + h*sf//2))
-    #fill in missing pixels, only fill area with content
-    distort_fill = correct_blanks(distort, box_to_fill[0], box_to_fill[1])
+
+    distort_fill = correct_blanks(distort, transformed_points)
     
      #writting to file - with blank filling
     position = ((int) (h*sf *8//8), (int) (w*sf * 4//8))
-    name = "K1="+str(K1)+"_K2="+str(K2)+"_K3="+str(K3)
     wtext = cv2.putText(distort_fill, name, 
         position,cv2.FONT_HERSHEY_SIMPLEX,1,(0, 255, 0), 4)
     cv2.imwrite(distored_link +"/"+img_pick+name+"fill.jpg", wtext)
 
     return distort_fill
 
-def correct_blanks(img, start, end):
+def correct_blanks(img,points):
     h,w,d = img.shape
     #much larger and more thorough - creates sharper image
     #adj = ((1,1), (1,-1), (-1,-1), (-1,1), (1,0),(0,1),(-1,0),(0,-1), (2,2), (2,-2), (-2,-2), (-2,2), (2,0),(0,2))
     #faster fill
-    adj = ((1,1), (1,-1), (-1,-1), (-1,1),(1,0),(0,1),(-1,0),(0,-1))
-    for y in range(start[1],end[1]):
-        for x in range(start[0],end[0]):
-            col_avg = np.array([0,0,0])
-            count  = 0
-            if (not np.array_equal(img[y,x], np.array([0,0,0]))):
+    adj = ((1,0),(0,1),(-1,0),(0,-1))
+    for pt in points:
+        col_avg = np.array([0,0,0])
+        count  = 0
+        try:
+            if (not np.array_equal(img[pt[0],pt[1]], np.array([0,0,0]))):
                 print("non black")
                 continue
-            for i in adj:
-                try:
-                    adj_p = img[np.add((y,x),i)[0], np.add((y,x),i)[1]]
-                    if (not np.array_equal(adj_p, np.array([0,0,0]))):
-                        col_avg += adj_p
-                        count += 1
-                except:
-                    print("no adj")
-            if (count > 0):
-                img[y,x] = np.divide(col_avg,count)
+        except:
+            print("not in image")
+        for i in adj:
+            try:
+                adj_p = img[np.add((pt[0],pt[1]),i)[0], np.add((pt[0],pt[1]),i)[1]]
+                if (not np.array_equal(adj_p, np.array([0,0,0]))):
+                    col_avg += adj_p
+                    count += 1
+            except:
+                print("no adj")
+        if (count > 0):
+            try:
+                img[pt[0],pt[1]] = np.divide(col_avg,count)
+            except:
+                print("pixel not in frame")
     return img
 
 
 def undistort_image(img):
     h,w,d = img.shape
     #scallling factor
-    sf = 2
-    undx = round(w/sf)
-    undy = round(h/sf)
-    undistort = np.zeros(shape=[undy,undx, 3], dtype=np.uint8)
-    for y in range(0,undy):
-        for x in range(0,undx):
-            xcorr = x-undx//2
-            ycorr = y-undy//2
-            r = math.sqrt((xcorr)**2 + (ycorr)**2)
-            try:
-                undistort[y][x] = undistort[cord_undistort(ycorr,r)+h*sf//2][cord_undistort(xcorr,r)+w*sf//2]
-            except:
-                print("index out of bound - distort_image")
-
+    sf = 1
+    undistort = np.zeros(shape=[round(h*sf), round(w*sf), 3], dtype=np.uint8)
+    x_cent = w//2
+    y_cent = h//2
     
+    for y in range(0,h):
+        for x in range(0,w):
+            x_norm = (x-x_cent)/x_cent
+            y_norm = (y-y_cent)/y_cent 
+            r = np.sqrt(x_norm**2 + y_norm**2)
+            x_undist_norm = cord_distort(x_norm,r)
+            y_undist_norm = cord_distort(y_norm,r)
+            x_undistorted = int(x_dist_norm*x_cent + x_cent) 
+            y_undistorted = int(y_dist_norm*y_cent + y_cent)
+            try: 
+                undistort[x][y]=img[y_undistorted][x_undistorted]
+            except:
+                print("OOB")
      #writting to file - with blank filling
     position = ((int) (h*sf *8//8), (int) (w*sf * 4//8))
-    name = "K1="+str(K1)+"_K2="+str(K2)+"_K3="+str(K3)
     wtext = cv2.putText(undistort, name, 
         position,cv2.FONT_HERSHEY_SIMPLEX,1,(0, 255, 0), 4)
     cv2.imwrite(undistored_link +"/"+img_undistpick+name+"undist.jpg", wtext)
@@ -120,10 +145,10 @@ def undistort_image(img):
     return undistort
 
 def cord_distort(c,r):
-    return math.floor(c * (1+ K1 * r**2 + K2 * r**4 + K3 * r**6))
+    return c*(1+K1*r**2 + K2*r**4 + K3*r**6)/x_scale
 
 def cord_undistort(c,r):
-    return math.floor(c / (1+ K1 * r**2 + K2 * r**4 + K3 * r**6))
+    return c*x_scale/(1+K1*r**2 + K2*r**4 + K3*r**6)
 
 def distpic():
     disp = distort_image(img)
@@ -139,8 +164,9 @@ def undistpic():
 
 
 if __name__ == "__main__":
-    #distpic()
-    undistpic()
+    #chae_shit(img)
+    distpic()
+    #undistpic()
 
 
 
